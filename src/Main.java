69452d0451604ea450e124b6a4c7f5ddad156fd2
Oksana Tarasenko <ksushatarasenko@gmail.com>
2023-01-06 11:05:07 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int x = 5;
+        double y = 2.5;
+        char sim = '&';
+        boolean student = true;
+
+
+
+        int a = 523;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(sim);
+        System.out.println("Student " + student );
+
+        System.out.println( a ); // вывод самого числа
+        System.out.println( a%1000/100); //вывод сотен
+        System.out.println( a%100/10); //вывод десятков
+        System.out.println( a%10);// вывод единиц
+
+
+
+    }
+}
